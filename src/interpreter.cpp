b9042d0479dd0855a8e7c8ef41fa6a232542f9f9
Touchebag <someone90_6@hotmail.com>
2015-10-09 21:27:13 +0200

    Started on main interpreter function

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -1,38 +1,51 @@
 #include "../inc/interpreter.h"
 
+#ifdef DEBUG
+// Add extra stuff for debugging
+#include "stdlib.h"
+#define VERBOSE
+using namespace std;
+#endif
+
 Interpreter::Interpreter(Registers *registers, Cpu_ram *ram) {
   reg = registers;
   memory = ram;
 };
 
+Interpreter::~Interpreter() {
+};
+
 void Interpreter::set_pc(BYTE low, BYTE high) {
   reg->pc[0] = low;
   reg->pc[1] = high;
-  std::cout << (int)reg->pc[1] << (int)reg->pc[0] << "\n";
 };
 
 // TODO Very ugly. Neccessary?
 void Interpreter::inc_pc(int number) {
   for (int i = 0; i < number; i++) {
     if (reg->pc[0] == 0xFF) {
-      reg->pc[0] = 0x00;
-      if (reg->pc[1] == 0xFF) {
-        reg->pc[1] = 0x00;
-      } else {
-        reg->pc[1]++;
-      };
-    } else {
-      reg->pc[0]++;
+      reg->pc[1] = (reg->pc[1] + 1) % 256;
     };
+    reg->pc[0] = (reg->pc[0] + 1) % 256;
   };
-
-  std::cout << (int)reg->pc[1] << (int)reg->pc[0] << "\n";
-
-};
-
-Interpreter::~Interpreter() {
 };
 
 int Interpreter::execute_instruction() {
+  int opcode = memory->read_ram(reg->pc[0], reg->pc[1]);
+
+  switch (opcode) {
+    case 0:
+      inc_pc(1);
+#ifdef VERBOSE
+      cout << opcode << " NOP\n";
+#endif
+      break;
+    default:
+#ifdef VERBOSE
+      cout << "Unknown opcode " << opcode;
+      exit(1);
+#endif
+      break;
+  };
   return OK;
 };

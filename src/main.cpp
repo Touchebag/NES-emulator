0b7b18de3ba2ab33b78523d08213a7a0d00e778a
Touchebag <someone90_6@hotmail.com>
2015-10-10 17:52:21 +0200

    Can now run roms from command line

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
+#include "../inc/system.h"
+#include <windows.h>
 #include <iostream>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
+  if (argc == 2) {
+    System *emulator = new System(argv[1]);
+    emulator->run();
+  };
+
+  system("pause");
   return 0;
-}
+};
+

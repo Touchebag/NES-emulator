0f94e72255c2ebeb8b52c2d9d43bbcd8be223e7e
Touchebag <someone90_6@hotmail.com>
2015-10-14 16:33:59 +0200

    Added opcodes A2 and BD

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -3,6 +3,7 @@
 #ifdef DEBUG
 // Add extra stuff for debugging
 #include "stdlib.h"
+#include <iomanip>
 #define VERBOSE
 using namespace std;
 #endif
@@ -51,31 +52,124 @@ void Interpreter::set_zero_bit(BYTE value) {
   set_status_bit(ZERO_FLAG, (value == 0));
 };
 
+/* {{{ Main interpreter loop */
 int Interpreter::execute_instruction() {
   int opcode = read_from_pc();
+  int cycles = 0;
+
+  /* Helper variables to avoid declaring new variables in switch statement
+   * Also to keep track of what they are used for
+   *
+   * hi/lo -> Memory addresses
+   * c     -> constants
+   * a,x.y -> contents of registers
+   */
+  BYTE hi, lo, c, a, x, y;
 
   switch (opcode) {
     /********  LDA  ********/
-    case 0xA9: { // Immediate
+    case 0xA9: // Immediate
       inc_pc(1);
-      BYTE c = read_from_pc();
-      reg->a = c;
+      a = read_from_pc();
+      reg->a = a;
+      inc_pc(1);
+
+      // Set status
+      set_negative_bit(a);
+      set_zero_bit(a);
+
+      // Number of cycles
+      cycles = 2;
+
+#ifdef VERBOSE
+      cout << std::hex << opcode << " LDA #" << (int)c << "\n";
+#endif
+      break;
+    case 0xBD: // Absolute, X
+      // Cycles first for easier adding of potential page-crossing cycle
+      cycles = 4;
+
+      inc_pc(1);
+      lo = read_from_pc();
+      inc_pc(1);
+      hi = read_from_pc();
+      inc_pc(1);
+      x = reg->x;
+
+      // If no page crossing
+      if (x < (256 - lo)) {
+        lo += x;
+      } else {
+        lo = (lo + x) % 255;
+        hi++;
+        cycles++;
+      }
+
+      // Store to A
+      a = memory->read_ram(lo, hi);
+      reg->a = a;
+
+      // Set status
+      set_negative_bit(a);
+      set_zero_bit(a);
+
+#ifdef VERBOSE
+      cout << std::hex << opcode << " LDA "
+        << setfill('0') << setw(2) << (int)hi
+        << setfill('0') << setw(2) << (int)lo
+        << " " << (int)x << " " << (int)a << "\n";
+#endif
+      break;
+
+    /********  LDX  ********/
+    case 0xA2: // Immediate
+      inc_pc(1);
+      c = read_from_pc();
+      reg->x = c;
       inc_pc(1);
 
       // Set status
       set_negative_bit(c);
       set_zero_bit(c);
 
+      // Number of cycles
+      cycles = 2;
+
 #ifdef VERBOSE
-      cout << "LDA #" << std::hex << (int)c << "\n";
+      cout << std::hex << opcode << " LDX #" << (int)c << "\n";
 #endif
-      break; }
+      break;
+
+    /********  STA  ********/
+    case 0x8D: // Absolute
+      inc_pc(1);
+      lo = read_from_pc();
+      inc_pc(1);
+      hi = read_from_pc();
+      inc_pc(1);
+
+      memory->write_ram(lo, hi, reg->a);
+
+      // Number of cycles
+      cycles = 3;
+
+#ifdef VERBOSE
+      cout << std::hex << opcode << " STA "
+        << setfill('0') << setw(2) << (int)hi
+        << setfill('0') << setw(2) << (int)lo
+        << " " << (int)reg->a << "\n";
+#endif
+      break;
     default:
 #ifdef VERBOSE
-      cout << "Unknown opcode " << std::hex << opcode;
+      cout << "Unknown opcode " << std::hex << (int)opcode;
       exit(1);
 #endif
       break;
   };
+
+  // ppu.cycle(cycles);
+
   return OK;
 };
+/* }}} */

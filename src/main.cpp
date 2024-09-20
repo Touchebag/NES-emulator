b3be315451c471558aff6fdc9417c12b684c7dcb
Touchebag <someone90_@hotmail.com>
2024-09-20 22:38:19 +0200

    Stop catching exceptions in main

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,11 @@
 #include <SFML/Graphics.hpp>
 
 int main(int argc, char *argv[]) {
-    try {
-        if (argc == 2) {
-            System::getInstance().loadRom(argv[1]);
+    if (argc == 2) {
+        System::getInstance().loadRom(argv[1]);
 
-            sf::RenderWindow window(sf::VideoMode(System::WINDOW_WIDTH, System::WINDOW_HEIGHT), "NES");
-            System::getInstance().run(window);
-        }
-    } catch (std::exception& e) {
-        exit(1);
+        sf::RenderWindow window(sf::VideoMode(System::WINDOW_WIDTH, System::WINDOW_HEIGHT), "NES");
+        System::getInstance().run(window);
     }
 
     return 0;
